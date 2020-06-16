3f5a051abff8e69284364c61eeba2a2cc54b3d4a
Eric Acosta Vega~ <eacosta901@gmail.com>
2020-06-16 11:26:19 -0500

    first commit

---
+++ src/app/components/dona/dona.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Label, MultiDataSet } from 'ng2-charts';
+import { ChartType } from 'chart.js';
+
+@Component({
+  selector: 'app-dona',
+  templateUrl: './dona.component.html',
+  styles: [
+  ]
+})
+export class DonaComponent  {
+
+  public doughnutChartLabels: Label[] = ['Tamales', 'Tacos', 'Tortillas'];
+  public doughnutChartData: MultiDataSet = [
+    [350, 450, 100],
+    [50, 150, 120],
+    [250, 130, 70],
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  // events
+  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+}
